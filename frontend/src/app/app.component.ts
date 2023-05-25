fdc269643701ddc36b5edb695ae13c55597c415d
Bartosz Wojtal <bartek@MacBook-Pro-Bartosz.local>
2023-05-25 15:16:51 +0200

    frontend preps

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { apiUrl } from 'src/assets/apiurl';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
   title = 'equip-master';
+
+  suppliers: any[] = [];
+
+  ngOnInit(): void {
+    this.http.get<any>(`${apiUrl}suppliers`).subscribe((sup) => {
+      console.log(sup.supplier);
+      this.suppliers = sup.supplier;
+    });
+  }
 }
---
+++ frontend/src/assets/apiurl.ts
@@ -0,0 +1,1 @@
+export const apiUrl: string = 'http://localhost:3511/api/v1/';
