3ff51957017a1b284f852429621cf6104cd1cf3a
Bartosz Wojtal <mrcraftize@gmail.com>
2024-05-18 23:41:30 +0200

    #29 Added delivery details component

---
+++ frontend/src/app/modules/core/delivery/pages/delivery-details-page/delivery-details-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delivery-details-page',
+  templateUrl: './delivery-details-page.component.html',
+  styleUrl: './delivery-details-page.component.scss'
+})
+export class DeliveryDetailsPageComponent {
+
+}
