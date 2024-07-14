887580e78a2dd4b38b978403196cda4a0558e07f
Bartosz Wojtal <mrcraftize@gmail.com>
2024-07-14 13:07:48 +0200

    #14 Adjusted graph component to be responsive

--- frontend/src/app/modules/core/home/components/dashboard-graph/dashboard-graph.component.ts
+++ frontend/src/app/modules/core/home/components/dashboard-graph/dashboard-graph.component.ts
@@ -16,29 +16,41 @@ export class DashboardGraphComponent implements OnInit {
   }
 
   createChart(data: DeliveryGraphDTO[]): void {
-
     const ctx = (document.getElementById('myChart') as HTMLCanvasElement).getContext('2d')!;
-    const chart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: data.map(dto => `${new Date(0, dto.month).toLocaleString('default', { month: 'long' })} ${dto.year}`),
-        datasets: [{
-          label: 'Liczba dostaw',
-          data: data.map(dto => dto.deliveries.length),
-          borderColor: 'rgb(75,157,192)',
-          // backgroundColor: 'rgb(75,157,192)',
-          borderWidth: 1,
-          fill: true
-        }]
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
+    let chart: Chart;
+
+    const updateChart = () => {
+      if (chart) {
+        chart.destroy();
+      }
+
+      chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: data.map(dto => `${new Date(0, dto.month).toLocaleString('default', { month: 'long' })} ${dto.year}`),
+          datasets: [{
+            label: 'Liczba dostaw',
+            data: data.map(dto => dto.deliveries.length),
+            borderColor: 'rgb(75,157,192)',
+            borderWidth: 1,
+            fill: true
+          }]
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          scales: {
+            y: {
+              beginAtZero: true
+            }
           }
         }
-      }
-    });
-    console.log(chart)
+      });
+    };
+
+    updateChart();
+
+    window.addEventListener('resize', updateChart);
   }
+
 }
