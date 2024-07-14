db9443963bd1d51ea9e14948eeb7426a5a623c05
Bartosz Wojtal <mrcraftize@gmail.com>
2024-07-14 13:40:48 +0200

    #14 Removed max width of graph and added functionality to navigate to delivery from dashboard

--- frontend/src/app/modules/core/home/components/dashboard-upcoming-deliveries/dashboard-upcoming-deliveries.component.ts
+++ frontend/src/app/modules/core/home/components/dashboard-upcoming-deliveries/dashboard-upcoming-deliveries.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { UpcomingDelivery } from "../../models/UpcomingDelivery";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-dashboard-upcoming-deliveries',
@@ -7,8 +8,13 @@ import { UpcomingDelivery } from "../../models/UpcomingDelivery";
   styleUrl: './dashboard-upcoming-deliveries.component.scss'
 })
 export class DashboardUpcomingDeliveriesComponent implements OnInit {
+  private router = inject(Router);
   @Input() upcomingDeliveries: UpcomingDelivery[];
 
   ngOnInit() {
+  }
+
+  navigateToDelivery(deliveryId: string) {
+    this.router.navigate(['delivery','create', deliveryId, 'counting'])
   }
 }
